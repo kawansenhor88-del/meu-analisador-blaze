dd85632fcf2356376fd36f8ef376e1a5e41d7108
kawansenhor88-del <kawansenhor88@gmail.com>
2026-09-07 11:42:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1243,9 +1243,9 @@ AJUSTE_FINAL_SURF = {
     500:  {"esquerda": -0.2, "direita": -0.4, "separador": 0.0},
     600:  {"esquerda": -0.2, "direita": -0.4, "separador": 0.0},
     700:  {"esquerda": -0.2, "direita": -0.4, "separador": 0.0},
-    800:  {"esquerda": 0.0, "direita": 0.0, "separador": 0.0},
-    900:  {"esquerda": -0.2, "direita": -0.2, "separador": -0.2},
-    1000: {"esquerda": 0.0, "direita": 0.0, "separador": 0.0},
+    800:  {"esquerda": -0.2, "direita": -0.1, "separador": -0.2},
+    900:  {"esquerda": -0.2, "direita": -0.1, "separador": -0.2},
+    1000: {"esquerda": -0.2, "direita": -0.1, "separador": -0.2},
 }
 
 def _montar_surfe_duas_colunas(registros, numero_final_analise=None):
